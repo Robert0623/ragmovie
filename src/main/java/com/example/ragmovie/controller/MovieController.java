8d1915503e957bcd8125a7febd238b092695be00
hwoo <tone8943@gmail.com>
2025-03-23 22:23:59 +0900

    view 추가

--- src/main/java/com/example/ragmovie/controller/MovieController.java
+++ src/main/java/com/example/ragmovie/controller/MovieController.java
@@ -1,4 +1,23 @@
 package com.example.ragmovie.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MovieController {
+
+    private final VectorStore vectorStore;
+    private final ChatClient chatClient;
+
+    public MovieController(VectorStore vectorStore, ChatClient.Builder chatClient) {
+        this.vectorStore = vectorStore;
+        this.chatClient = chatClient.build();
+    }
+
+    @GetMapping("/movie")
+    public String getRecommendationForm() {
+        return "movieRAG";
+    }
 }
