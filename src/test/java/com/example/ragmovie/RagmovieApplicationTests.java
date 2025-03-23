77df1dbaa1b1ad78e66245f51bb545772896b077
hwoo <tone8943@gmail.com>
2025-03-23 19:59:10 +0900

    Initial commit

---
+++ src/test/java/com/example/ragmovie/RagmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ragmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagmovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
